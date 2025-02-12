4559e28d1973fc012a1f8d4d5d6b768c548e8d00
Victor Abussafi <victorabussafi@gmail.com>
2025-02-12 10:33:16 +0000

    implement new project creation functionality

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom"
 import Navbar from "./components/Navbar"
 import HomePage from "./pages/Homepage"
 import ProjectListPage from "./pages/ProjectListPage"
+import CreateProjectPage from "./pages/CreateProjectPage"
 
 function App() {
   
@@ -14,6 +15,8 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage/>} />
         <Route path="/projects" element={<ProjectListPage></ProjectListPage>} />
+        <Route path="/projects/create" element={<CreateProjectPage/>} />
+       
       </Routes>
 
       
