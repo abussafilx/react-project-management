f7b158da86ca486a40b29ab0b6af6a6910d63ca0
Victor Abussafi <victorabussafi@gmail.com>
2025-02-12 10:58:52 +0000

    display project list in reverse

--- src/pages/ProjectListPage.jsx
+++ src/pages/ProjectListPage.jsx
@@ -11,7 +11,7 @@ function ProjectListPage() {
     useEffect(() => {
         axios.get(`${API_URL}/projects`)
             .then( response => {
-                setProjects(response.data);
+                setProjects(response.data.toReversed());
             })
             .catch(e => console.log("Error getting projects from the API...", e));
     }, []);
